2e605cd3c296b1e51d6f3de065f9fc963d95dfc4
cats256 <59489624+cats256@users.noreply.github.com>
2023-11-21 01:51:16 -0600

    Add email sending utility

---
+++ backend/src/util/sendEmail.ts
@@ -0,0 +1,28 @@
+import * as nodemailer from 'nodemailer';
+function sendEmailNotification(toEmail: string, subject: string, text: string): void {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'yourEmail@gmail.com',
+            pass: 'yourPassword'
+        }
+    });
+
+    const mailOptions: nodemailer.SendMailOptions = {
+        from: 'yourEmail@gmail.com',
+        to: toEmail,
+        subject: subject,
+        text: text
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+// Usage example
+sendEmailNotification('recipient@example.com', 'Test Subject', 'This is a test email.');
