4cabb8b1aac1ec571a574ae25f7aeee9afc3afc6
cats256 <59489624+cats256@users.noreply.github.com>
2023-11-25 22:15:15 -0600

    Update frontend to display scraping data

--- frontend/src/network/objs_api.ts
+++ frontend/src/network/objs_api.ts
@@ -1,103 +1,109 @@
 import { ConflictError, UnauthorizedError } from "../errors/http_errors";
 import { Obj } from "../models/object";
 import { User } from "../models/user";
+import { scrapeWebsite } from "./scrape_api";
 
-async function fetchData(input: RequestInfo, init?: RequestInit) {
-  const response = await fetch(input, init);
-  if (response.ok) {
-    return response;
-  } else {
-    const errorBody = await response.json();
-    const errorMessage = errorBody.error;
-    if (response.status === 401) {
-      throw new UnauthorizedError(errorMessage);
-    } else if (response.status === 409) {
-      throw new ConflictError(errorMessage);
+export async function fetchData(input: RequestInfo, init?: RequestInit) {
+    const response = await fetch(input, init);
+    if (response.ok) {
+        return response;
     } else {
-      throw Error(
-        "Request failed with status: " +
-          response.status +
-          " message: " +
-          errorMessage
-      );
+        const errorBody = await response.json();
+        const errorMessage = errorBody.error;
+        if (response.status === 401) {
+            throw new UnauthorizedError(errorMessage);
+        } else if (response.status === 409) {
+            throw new ConflictError(errorMessage);
+        } else {
+            throw Error("Request failed with status: " + response.status + " message: " + errorMessage);
+        }
     }
-  }
 }
 
 export async function getLoggedInUser(): Promise<User> {
-  const response = await fetchData("/api/users", { method: "GET" });
-  return response.json();
+    const response = await fetchData("/api/users", { method: "GET" });
+    return response.json();
 }
 
 export interface SignUpCredentials {
-  username: string;
-  email: string;
-  password: string;
+    username: string;
+    email: string;
+    password: string;
 }
 
 export async function signUp(credentials: SignUpCredentials): Promise<User> {
-  const response = await fetchData("/api/users/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  });
-  return response.json();
+    const response = await fetchData("/api/users/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    });
+    return response.json();
 }
 
 export interface LoginCredentials {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 }
 
 export async function login(credentials: LoginCredentials): Promise<User> {
-  const response = await fetchData("/api/users/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  });
-  return response.json();
+    const response = await fetchData("/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    });
+    return response.json();
 }
 
 export async function logout() {
-  await fetchData("/api/users/logout", { method: "POST" });
+    await fetchData("/api/users/logout", { method: "POST" });
 }
 
 export async function fetchObjs(): Promise<Obj[]> {
-  const response = await fetchData("/api/objs", { method: "GET" });
-  return response.json();
+    const response = await fetchData("/api/objs", { method: "GET" });
+    return response.json();
 }
 
 export interface ObjInput {
-  title: string;
-  text?: string;
+    title: string;
+    text?: string;
 }
 
 export async function createObj(obj: ObjInput): Promise<Obj> {
-  const response = await fetchData("/api/objs", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(obj),
-  });
-  return response.json();
+    const response = await fetchData("/api/objs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(obj),
+    });
+
+    const user = await getLoggedInUser();
+    const url = obj.title;
+    const scrapedData = await scrapeWebsite(user._id, url);
+
+    const response_json = await response.json();
+    for (let i = 0; i < scrapedData.length; i++) {
+        response_json.text += scrapedData[i] + "\n";
+    }
+
+    return response_json;
 }
 
 export async function updateObj(objId: string, obj: ObjInput) {
-  const response = await fetchData("/api/objs/" + objId, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(obj),
-  });
-  return response.json();
+    const response = await fetchData("/api/objs/" + objId, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(obj),
+    });
+    return response.json();
 }
 
 export async function deleteObj(objId: string) {
-  await fetchData("/api/objs/" + objId, { method: "DELETE" });
+    await fetchData("/api/objs/" + objId, { method: "DELETE" });
 }
---
+++ frontend/src/network/scrape_api.ts
@@ -0,0 +1,34 @@
+import * as ObjsApi from "./objs_api";
+
+export async function updateConfig(userId: string, url: string, parameter: string): Promise<any> {
+    const response = await ObjsApi.fetchData("/api/scrape/updateConfig", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            userId,
+            url,
+            parameters: [parameter],
+        }),
+    });
+
+    return (await response.json())._id;
+}
+
+export async function scrapeWebsite(userId: string, url: string, parameter: string = "owner-sub-count"): Promise<any> {
+    const configId = await updateConfig(userId, url, parameter);
+
+    console.log(configId);
+    const response = await ObjsApi.fetchData("/api/scrape/scrape", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            configId,
+        }),
+    });
+
+    return response.json();
+}
