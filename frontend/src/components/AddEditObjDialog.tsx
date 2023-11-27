eb55633416c282531f96762da388684d6ff5e99d
cats256 <59489624+cats256@users.noreply.github.com>
2023-11-27 17:27:30 -0600

    Update form values and change to readonly

--- frontend/src/components/AddEditObjDialog.tsx
+++ frontend/src/components/AddEditObjDialog.tsx
@@ -6,103 +6,101 @@ import * as ObjApi from "../network/objs_api";
 import TextInputField from "./form/TextInputField";
 
 interface AddEditObjDialogProps {
-  objToEdit?: Obj;
-  onDismiss: () => void;
-  onObjSaved: (obj: Obj) => void;
+    objToEdit?: Obj;
+    onDismiss: () => void;
+    onObjSaved: (obj: Obj) => void;
 }
 
-const AddEditObjDialog = ({
-  objToEdit,
-  onDismiss,
-  onObjSaved,
-}: AddEditObjDialogProps) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<ObjInput>({
-    defaultValues: {
-      title: objToEdit?.title || "",
-      text: objToEdit?.text || "",
-    },
-  });
+const AddEditObjDialog = ({ objToEdit, onDismiss, onObjSaved }: AddEditObjDialogProps) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<ObjInput>({
+        defaultValues: {
+            title: objToEdit?.title || "",
+            text: objToEdit?.text || "",
+        },
+    });
 
-  async function onSubmit(input: ObjInput) {
-    try {
-      let objResponse: Obj;
-      if (objToEdit) {
-        objResponse = await ObjApi.updateObj(objToEdit._id, input);
-      } else {
-        objResponse = await ObjApi.createObj(input);
-      }
-      onObjSaved(objResponse);
-    } catch (error) {
-      console.error(error);
-      alert(error);
+    async function onSubmit(input: ObjInput) {
+        try {
+            let objResponse: Obj;
+            if (objToEdit) {
+                objResponse = await ObjApi.updateObj(objToEdit._id, input);
+            } else {
+                objResponse = await ObjApi.createObj(input);
+            }
+            onObjSaved(objResponse);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
     }
-  }
 
-  return (
-    <Modal show onHide={onDismiss}>
-      <Modal.Header closeButton>
-        <Modal.Title>{objToEdit ? "Edit Obj" : "Add Obj"}</Modal.Title>
-      </Modal.Header>
+    return (
+        <Modal show onHide={onDismiss}>
+            <Modal.Header closeButton>
+                <Modal.Title>{objToEdit ? "Edit Object" : "Add Object"}</Modal.Title>
+            </Modal.Header>
 
-      <Modal.Body>
-        <Form id="addEditObjForm" onSubmit={handleSubmit(onSubmit)}>
-          <TextInputField
-            name="title"
-            label="Title"
-            type="text"
-            placeholder="Title"
-            register={register}
-            registerOptions={{ required: "Required" }}
-            error={errors.title}
-          />
+            <Modal.Body>
+                <Form id="addEditObjForm" onSubmit={handleSubmit(onSubmit)}>
+                    <TextInputField
+                        name="title"
+                        label="Website URL"
+                        type="text"
+                        placeholder="Title"
+                        register={register}
+                        registerOptions={{ required: "Required" }}
+                        error={errors.title}
+                    />
 
-          <TextInputField
-            name="text"
-            label="Text"
-            as="textarea"
-            rows={5}
-            placeholder="Text"
-            register={register}
-          />
-        </Form>
-      </Modal.Body>
+                    <TextInputField
+                        name="text"
+                        label="Text"
+                        as="textarea"
+                        rows={5}
+                        placeholder="Text"
+                        register={register}
+                        readOnly
+                    />
+                </Form>
+            </Modal.Body>
 
-      <Modal.Footer>
-        <Button
-          type="submit"
-          form="addEditObjForm"
-          disabled={isSubmitting}
-          style={{
-            backgroundColor: "#164863",
-            transition: "background-color 0.3s",
-            borderColor: "#427d9d",
-            borderWidth: "1px",
-          }}
-          onMouseDown={(e) => {
-            e.currentTarget.style.backgroundColor = "#9bbec8";
-            e.currentTarget.style.borderColor = "#9bbec8";
-            e.currentTarget.style.borderWidth = "2px";
-          }}
-          onMouseUp={(e) => {
-            e.currentTarget.style.backgroundColor = "#427d9d";
-            e.currentTarget.style.borderWidth = "1px";
-          }}
-          onMouseEnter={(e) => {
-            e.currentTarget.style.backgroundColor = "#427d9d";
-          }}
-          onMouseLeave={(e) => {
-            e.currentTarget.style.backgroundColor = "#164863";
-            e.currentTarget.style.borderWidth = "1px";
-          }}>
-          Save
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+            <Modal.Footer>
+                <Button
+                    type="submit"
+                    form="addEditObjForm"
+                    disabled={isSubmitting}
+                    style={{
+                        backgroundColor: "#164863",
+                        transition: "background-color 0.3s",
+                        borderColor: "#427d9d",
+                        borderWidth: "1px",
+                    }}
+                    onMouseDown={(e) => {
+                        e.currentTarget.style.backgroundColor = "#9bbec8";
+                        e.currentTarget.style.borderColor = "#9bbec8";
+                        e.currentTarget.style.borderWidth = "2px";
+                    }}
+                    onMouseUp={(e) => {
+                        e.currentTarget.style.backgroundColor = "#427d9d";
+                        e.currentTarget.style.borderWidth = "1px";
+                    }}
+                    onMouseEnter={(e) => {
+                        e.currentTarget.style.backgroundColor = "#427d9d";
+                    }}
+                    onMouseLeave={(e) => {
+                        e.currentTarget.style.backgroundColor = "#164863";
+                        e.currentTarget.style.borderWidth = "1px";
+                    }}
+                >
+                    Save
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default AddEditObjDialog;
