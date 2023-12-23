ba5124f99563a115d91495dbd6df408ab3861f1a
cats256 <59489624+cats256@users.noreply.github.com>
2023-12-23 16:49:31 -0600

    Update scrape config to pre save timetoscrape

--- backend/src/models/scrapeConfig.ts
+++ backend/src/models/scrapeConfig.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const scrapeConfigSchema = new Schema(
     {
-        user: { type: Schema.Types.ObjectId, ref: "User" },
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
         url: { type: String, required: true },
         parameters: { type: Schema.Types.Mixed, required: true },
         timeToScrape: { type: Date, required: true },
@@ -13,3 +13,8 @@ const scrapeConfigSchema = new Schema(
 
 const ScrapeConfig = model("ScrapeConfig", scrapeConfigSchema);
 export default ScrapeConfig;
+
+scrapeConfigSchema.pre("save", function (next) {
+    this.timeToScrape = new Date(Date.now() + this.scrapeIntervalMinute * 60000);
+    next();
+});
