110ef21229ba9069c7261b8105148846d977b7be
cats256 <59489624+cats256@users.noreply.github.com>
2023-12-23 14:31:56 -0600

    Add playground commands to add index to scrapeconfig

---
+++ playground-1.mongodb.js
@@ -0,0 +1,9 @@
+// use("ws_app_db");
+// show("collections");
+// db.scrapeconfigs.getIndexes();
+
+// db.scrapeconfigs.createIndex({ timeToScrape: 1 });
+
+// delete all documents in scrapconfigs collection
+// DO NOT RUN THIS COMMAND OR ANYTHING SIMILAR UNLESS YOU KNOW WHAT YOU ARE DOING
+// db.scrapeconfigs.deleteMany({});
