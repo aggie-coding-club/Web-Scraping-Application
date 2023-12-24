10b072485058ce0d89603ce38fabab287e94c2fb
cats256 <59489624+cats256@users.noreply.github.com>
2023-12-23 22:09:53 -0600

    Update scraping error handling

--- backend/src/util/scrapeWebsite.ts
+++ backend/src/util/scrapeWebsite.ts
@@ -1,28 +1,29 @@
 import puppeteer from "puppeteer";
 
 export const scrapeWebsite = async (url: string, parameter_obj: ScrapingConfigObject) => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto(url);
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url);
 
-  const selectors = Object.values(parameter_obj);
-  const timeoutPromise = new Promise((resolve) => setTimeout(resolve, 10 * 1000));
-  
-  await Promise.race([
-    Promise.all(selectors.map(selector => page.waitForSelector(selector))),
-    timeoutPromise
-  ])
+    const selectors = Object.values(parameter_obj);
+    const timeoutPromise = new Promise((resolve) => setTimeout(resolve, 10 * 1000));
 
-  const scrapedData = await page.evaluate((params) => {
-    const results: ScrapingConfigObject = {};
-    Object.entries(params).forEach(([key, selector]) => {
-      const element = document.querySelector(selector);
-      const content = element ? element.textContent?.trim() || "" : "";
-      results[key] = content;
-    });
-    return results;
-  }, parameter_obj);
+    await Promise.race([Promise.all(selectors.map((selector) => page.waitForSelector(selector))), timeoutPromise]);
 
-  await browser.close();
-  return scrapedData;
+    const scrapedData = await page.evaluate((params) => {
+        const results: ScrapingConfigObject = {};
+        Object.entries(params).forEach(([key, selector]) => {
+            try {
+                const element = document.querySelector(selector);
+                const content = element ? element.textContent?.trim() || "" : "";
+                results[key] = content;
+            } catch (error) {
+                console.error("Error in scrapeWebsite:", error);
+            }
+        });
+        return results;
+    }, parameter_obj);
+
+    await browser.close();
+    return scrapedData;
 };
