fcb0884c1597261f511df65dcfb7d6bb777511fb
cats256 <59489624+cats256@users.noreply.github.com>
2023-12-24 21:55:28 -0600

    Update assertIsDefined to accept a custom message

--- backend/src/util/assertIsDefined.ts
+++ backend/src/util/assertIsDefined.ts
@@ -1,5 +1,5 @@
-export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
-  if (!val) {
-    throw Error("Expected 'val' to be defined, but received " + val);
-  }
+export function assertIsDefined<T>(val: T, message: string = "Expected 'val' to be defined, but received " + val): asserts val is NonNullable<T> {
+    if (!val || val === null || val === undefined) {
+        throw new Error(message);
+    }
 }
