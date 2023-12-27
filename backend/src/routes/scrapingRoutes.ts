5aea206d992bd61a607616d15aa796d5c7f28a33
Andrew Beketov <romanb996@gmail.com>
2023-12-27 05:21:33 -0600

    add ui rendering and element selection

--- backend/src/routes/scrapingRoutes.ts
+++ backend/src/routes/scrapingRoutes.ts
@@ -6,30 +6,75 @@ const router = express.Router();
 
 router.get("/getScrapingConfigs", scrapingController.getScrapingConfigs);
 
-router.post('/fetchHtml', (req, res) => {
-    const { url } = req.body;
+router.post("/fetchHtml", (req, res) => {
+  const { url } = req.body;
 
-    https.get(url, (response) => {
-        let data = '';
+  https
+    .get(url, (response) => {
+      let data = "";
 
-        // chunk
-        response.on('data', (chunk) => {
-            data += chunk;
-        });
+      // chunk
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
 
-        // whole response
-        response.on('end', () => {
-            res.send(data);
-        });
-    }).on("error", (err) => {
-        res.status(500).send(`Error fetching HTML content: ${err.message}`);
+      // whole response
+      response.on("end", () => {
+        const script = `
+        <script>
+            document.body.addEventListener('mouseover', function(event) {
+                event.target.style.outline = '2px solid blue';
+                event.stopPropagation();
+            }, true);
+    
+            document.body.addEventListener('mouseout', function(event) {
+                event.target.style.outline = '';
+                event.stopPropagation();
+            }, true);
+    
+            document.body.addEventListener('click', function(event) {
+                const selector = getCssSelector(event.target);
+                window.parent.postMessage({ selector }, '*');
+                event.preventDefault();
+                event.stopPropagation();
+            }, true);
+    
+            function getCssSelector(el) {
+                var path = [];
+                while (el.nodeType === Node.ELEMENT_NODE) {
+                    var selector = el.nodeName.toLowerCase();
+                    if (el.id) {
+                        selector += '#' + el.id;
+                        path.unshift(selector);
+                        break;
+                    }
+                    
+                    el = el.parentNode;
+                }
+                return path.join(' > ');
+            }
+        </script>
+    `;
+
+        const modifiedHtml = data + script;
+        res.send(modifiedHtml);
+      });
+    })
+    .on("error", (err) => {
+      res.status(500).send(`Error fetching HTML content: ${err.message}`);
     });
 });
 
 router.post("/createScrapingConfig", scrapingController.createScrapingConfig);
 
-router.patch("/updateScrapingConfig/:configId", scrapingController.updateScrapingConfig);
+router.patch(
+  "/updateScrapingConfig/:configId",
+  scrapingController.updateScrapingConfig
+);
 
-router.delete("/deleteScrapingConfig/:configId", scrapingController.deleteScrapingConfig);
+router.delete(
+  "/deleteScrapingConfig/:configId",
+  scrapingController.deleteScrapingConfig
+);
 
 export default router;
--- frontend/src/components/AddEditObjDialog.tsx
+++ frontend/src/components/AddEditObjDialog.tsx
@@ -1,155 +1,166 @@
+import React, { useState, useEffect } from "react";
 import { Modal, Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
-import { useState, useEffect } from "react";
 import { Obj } from "../models/object";
 import { ObjInput } from "../models/objInput";
 import * as ObjApi from "../network/objs_api";
 import TextInputField from "./form/TextInputField";
-import { fetchHtmlContent } from '../network/objs_api';
+import { fetchHtmlContent } from "../network/objs_api";
 
 interface AddEditObjDialogProps {
-    objToEdit?: Obj;
-    onDismiss: () => void;
-    onObjSaved: (obj: Obj) => void;
+  objToEdit?: Obj;
+  onDismiss: () => void;
+  onObjSaved: (obj: Obj) => void;
 }
 
-const AddEditObjDialog = ({ objToEdit, onDismiss, onObjSaved }: AddEditObjDialogProps) => {
-    const [iframeSrc, setIframeSrc] = useState('');
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors, isSubmitting },
-    } = useForm<ObjInput>({
-        defaultValues: {
-            url: objToEdit?.url || "",
-            scrapeParameters: JSON.stringify(objToEdit?.scrapeParameters, null, 2) || "",
-            scrapeIntervalMinute: objToEdit?.scrapeIntervalMinute || 1,
-        },
-    });
+const AddEditObjDialog = ({
+  objToEdit,
+  onDismiss,
+  onObjSaved,
+}: AddEditObjDialogProps) => {
+  const [iframeSrc, setIframeSrc] = useState("");
+  const [selectors, setSelectors] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<ObjInput>({
+    defaultValues: {
+      url: objToEdit?.url || "",
+      scrapeParameters:
+        JSON.stringify(objToEdit?.scrapeParameters, null, 2) || "",
+      scrapeIntervalMinute: objToEdit?.scrapeIntervalMinute || 1,
+    },
+  });
 
-    // Watch for URL changes
-    const url = watch("url");
+  const url = watch("url");
 
-    // Function to fetch HTML content and set as iframe source
-    // const fetchAndSetHtml = async (url: string) => {
-    //     try {
-    //         const response = await fetch('/scrape/fetchHtml', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({ url }),
-    //         });
-    //         const htmlContent = await response.text();
-    //         setIframeSrc(htmlContent);
-    //     } catch (error) {
-    //         console.error("Error fetching HTML: ", error);
-    //     }
-    // };
-
-    // Update iframe source when the URL changes
-    useEffect(() => {
-        if (url) {
-            fetchHtmlContent(url)
-                .then(htmlContent => {
-                    setIframeSrc(htmlContent);
-                })
-                .catch(error => console.error('Error fetching HTML:', error));
-        }
-    }, [url]);
-
-    async function onSubmit(input: ObjInput) {
-        try {
-            const objResponse = objToEdit ? await ObjApi.updateObj(objToEdit._id, input) : await ObjApi.createObj(input);
-            onObjSaved(objResponse);
-        } catch (error) {
-            console.error(error);
-            alert(error);
-        }
+  useEffect(() => {
+    if (url) {
+      fetchHtmlContent(url)
+        .then((htmlContent) => {
+          setIframeSrc(htmlContent);
+        })
+        .catch((error) => console.error("Error fetching HTML:", error));
     }
+  }, [url]);
 
-    return (
-        <Modal show onHide={onDismiss} size="xl"> {/* Change size to "xl" for extra large modal */}
-            <Modal.Header closeButton>
-                <Modal.Title>{objToEdit ? "Edit Object" : "Add Object"}</Modal.Title>
-            </Modal.Header>
-    
-            <Modal.Body>
-                <div style={{ display: "flex", flexDirection: "row", height: "80vh" }}> {/* Adjusted height */}
-                    <div style={{ flex: 1, paddingRight: "20px" }}> {/* Adjusted for flexible sizing */}
-                        <iframe 
-                            srcDoc={iframeSrc} 
-                            style={{ width: "100%", height: "100%", transform: "scale(1)", transformOrigin: "0 0" }} 
-                        />
-                    </div>
-                    <div style={{ flex: 1 }}>
-                        <Form id="addEditObjForm" onSubmit={handleSubmit(onSubmit)}>
-                            <TextInputField
-                                name="url"
-                                label="Website URL"
-                                type="text"
-                                placeholder="URL"
-                                register={register}
-                                registerOptions={{ required: "Required" }}
-                                error={errors.url}
-                            />
+  useEffect(() => {
+    const receiveMessage = (event) => {
+      if (event.origin !== window.location.origin) {
+        return;
+      }
+      if (event.data.selector) {
+        setSelectors((prevSelectors) => [
+          ...prevSelectors,
+          event.data.selector,
+        ]);
+      }
+    };
 
-                            <TextInputField
-                                name="scrapeParameters"
-                                label="Scrape Parameters"
-                                as="textarea"
-                                rows={4}
-                                placeholder="Text"
-                                register={register}
-                                registerOptions={{ required: "Required" }}
-                                error={errors.scrapeParameters}
-                            />
+    window.addEventListener("message", receiveMessage);
+    return () => window.removeEventListener("message", receiveMessage);
+  }, []);
 
-                            <TextInputField
-                                name="scrapeIntervalMinute"
-                                label="Scrape Interval (Minutes)"
-                                type="number"
-                                placeholder="Enter interval in minutes"
-                                register={register}
-                                registerOptions={{ required: "Required" }}
-                                error={errors.scrapeIntervalMinute}
-                            />
+  async function onSubmit(input: ObjInput) {
+    try {
+        const objResponse = objToEdit ? await ObjApi.updateObj(objToEdit._id, input) : await ObjApi.createObj(input);
+        onObjSaved(objResponse);
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+}
 
-                            <Button
-                                type="submit"
-                                disabled={isSubmitting}
-                                style={{
-                                    backgroundColor: "#164863",
-                                    transition: "background-color 0.3s",
-                                    borderColor: "#427d9d",
-                                    borderWidth: "1px",
-                                }}
-                                onMouseDown={(e) => {
-                                    e.currentTarget.style.backgroundColor = "#9bbec8";
-                                    e.currentTarget.style.borderColor = "#9bbec8";
-                                    e.currentTarget.style.borderWidth = "2px";
-                                }}
-                                onMouseUp={(e) => {
-                                    e.currentTarget.style.backgroundColor = "#427d9d";
-                                    e.currentTarget.style.borderWidth = "1px";
-                                }}
-                                onMouseEnter={(e) => {
-                                    e.currentTarget.style.backgroundColor = "#427d9d";
-                                }}
-                                onMouseLeave={(e) => {
-                                    e.currentTarget.style.backgroundColor = "#164863";
-                                    e.currentTarget.style.borderWidth = "1px";
-                                }}
-                            >
-                                Save
-                            </Button>
-                        </Form>
-                    </div>
-                </div>
-            </Modal.Body>
-        </Modal>
-    );
+  return (
+    <Modal show onHide={onDismiss} size="xl">
+      <Modal.Header closeButton>
+        <Modal.Title>{objToEdit ? "Edit Object" : "Add Object"}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div style={{ display: "flex", flexDirection: "row", height: "80vh" }}>
+          <div style={{ flex: 1, paddingRight: "20px" }}>
+            <iframe
+              srcDoc={iframeSrc}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+          <div style={{ flex: 1 }}>
+          <Form id="addEditObjForm" onSubmit={handleSubmit(onSubmit)}>
+              <TextInputField
+                name="url"
+                label="Website URL"
+                type="text"
+                placeholder="URL"
+                register={register}
+                registerOptions={{ required: "Required" }}
+                error={errors.url}
+              />
+              <TextInputField
+                name="scrapeParameters"
+                label="Scrape Parameters"
+                as="textarea"
+                rows={4}
+                placeholder="Text"
+                register={register}
+                registerOptions={{ required: "Required" }}
+                error={errors.scrapeParameters}
+              />
+              <TextInputField
+                name="scrapeIntervalMinute"
+                label="Scrape Interval (Minutes)"
+                type="number"
+                placeholder="Enter interval in minutes"
+                register={register}
+                registerOptions={{ required: "Required" }}
+                error={errors.scrapeIntervalMinute}
+              />
+
+              <div>
+                <h3>Captured Selectors</h3>
+                <ul>
+                  {selectors.map((selector, index) => (
+                    <li key={index}>{selector}</li>
+                  ))}
+                </ul>
+              </div>
+
+              <Button
+                    type="submit"
+                    form="addEditObjForm"
+                    disabled={isSubmitting}
+                    style={{
+                        backgroundColor: "#164863",
+                        transition: "background-color 0.3s",
+                        borderColor: "#427d9d",
+                        borderWidth: "1px",
+                    }}
+                    onMouseDown={(e) => {
+                        e.currentTarget.style.backgroundColor = "#9bbec8";
+                        e.currentTarget.style.borderColor = "#9bbec8";
+                        e.currentTarget.style.borderWidth = "2px";
+                    }}
+                    onMouseUp={(e) => {
+                        e.currentTarget.style.backgroundColor = "#427d9d";
+                        e.currentTarget.style.borderWidth = "1px";
+                    }}
+                    onMouseEnter={(e) => {
+                        e.currentTarget.style.backgroundColor = "#427d9d";
+                    }}
+                    onMouseLeave={(e) => {
+                        e.currentTarget.style.backgroundColor = "#164863";
+                        e.currentTarget.style.borderWidth = "1px";
+                    }}
+                >
+                Save
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export default AddEditObjDialog;
