f3ec1f9f71715afdf3b24263c97dc99d639dad7d
cats256 <59489624+cats256@users.noreply.github.com>
2024-02-10 15:29:46 -0600

    Add some code

---
+++ testing/frontend_remade/src/App.js
@@ -0,0 +1,87 @@
+import { useContext, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import LoginModal from "./components/LoginModal";
+// import NavBar from "./components/NavBar";
+// import Footer from "./components/Footer";
+// import SignUpModal from "./components/SignUpModal";
+// import { Sidebar } from "./components/Sidebar";
+
+// import ObjsPage from "./pages/ObjsPage";
+// import ExamplePage from "./pages/ExamplePage";
+// import SettingsPage from "./pages/SettingsPage";
+// import NotificationsPage from "./pages/NotificationsPage";
+// import AboutPage from "./pages/AboutPage";
+// import ContactPage from "./pages/ContactPage";
+// import NotFoundPage from "./pages/NotFoundPage";
+
+import UserContext from "./providers/UserProvider";
+import styles from "./styles/App.module.css";
+
+function App() {
+    const { setLoggedInUser } = useContext(UserContext);
+    const [showSignUpModal, setShowSignUpModal] = useState(false);
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    const [sidebarExpanded, setSidebarExpanded] = useState(false);
+    const [sidebarWidth, setSidebarWidth] = useState("65px");
+
+    const stylesContentWrapper = {
+        marginLeft: sidebarWidth,
+        transition: "margin-left 0.3s ease-in-out",
+    };
+
+    useEffect(() => {
+        const newWidth = sidebarExpanded ? "300px" : "65px";
+        setSidebarWidth(newWidth);
+    }, [sidebarExpanded]);
+
+    return (
+        <BrowserRouter>
+            <div className={styles.appContainer}>
+                {/* <Sidebar onToggle={setSidebarExpanded} /> */}
+                {/* <NavBar
+                    onLoginClicked={() => setShowLoginModal(true)}
+                    onSignUpClicked={() => setShowSignUpModal(true)}
+                    onLogoutSuccessful={() => setLoggedInUser(null)}
+                    sidebarExpanded={sidebarExpanded}
+                /> */}
+                <div className={styles.contentWrapper} style={stylesContentWrapper}>
+                    <div className={styles.mainContent}>
+                        <Routes>
+                            {/* <Route path="/" element={<ObjsPage />} />
+                            <Route path="/example" element={<ExamplePage />} />
+                            <Route path="/settings" element={<SettingsPage />} />
+                            <Route path="/notifications" element={<NotificationsPage />} />
+                            <Route path="/about" element={<AboutPage />} />
+                            <Route path="/contact" element={<ContactPage />} />
+                            <Route path="/*" element={<NotFoundPage />} /> */}
+                        </Routes>
+                    </div>
+                </div>
+                {showSignUpModal && (
+                    <></>
+                    // <SignUpModal
+                    //     onDismiss={() => setShowSignUpModal(false)}
+                    //     onSignUpSuccessful={(user) => {
+                    //         setLoggedInUser(user);
+                    //         setShowSignUpModal(false);
+                    //     }}
+                    // />
+                )}
+                {showLoginModal && (
+                    <></>
+                    // <LoginModal
+                    //     onDismiss={() => setShowLoginModal(false)}
+                    //     onLoginSuccessful={(user) => {
+                    //         setLoggedInUser(user);
+                    //         setShowLoginModal(false);
+                    //     }}
+                    // />
+                )}
+                {/* <Footer /> */}
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ testing/frontend_remade/src/providers/UserProvider.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, useEffect, ReactNode } from "react";
+import { User } from "../models/user";
+import * as ObjsApi from "../network/objs_api";
+
+type UserContextType = {
+    loggedInUser: User | null;
+    setLoggedInUser: (user: User | null) => void;
+};
+
+const UserContext = createContext<UserContextType>({
+    loggedInUser: null,
+    setLoggedInUser: () => {},
+});
+
+type UserProviderProps = {
+    children: ReactNode;
+};
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+    const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        async function fetchLoggedInUser() {
+            try {
+                const user = await ObjsApi.getLoggedInUser();
+                setLoggedInUser(user);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchLoggedInUser();
+    }, []);
+
+    return <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>{children}</UserContext.Provider>;
+};
+
+export const useUserContext = () => {
+    const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+
+    if (loggedInUser === null || setLoggedInUser === null) {
+        throw new Error("User is null. Use useContext(userContext) if user can be null.");
+    }
+    return { loggedInUser, setLoggedInUser };
+};
+
+export default UserContext;
