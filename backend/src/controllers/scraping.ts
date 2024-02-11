a14fd7899baaccc53e4a2edfdc23efcaea4d77d2
cats256 <59489624+cats256@users.noreply.github.com>
2024-02-11 00:01:23 -0600

    Update backend and frontend to properly add scraping config

--- backend/src/controllers/scraping.ts
+++ backend/src/controllers/scraping.ts
@@ -5,27 +5,6 @@ import { setNextScrapeTimeout } from "../util/checkAndExecuteScrape";
 import { createNote } from "./objects";
 import { assertIsDefined } from "../util/assertIsDefined";
 
-type ScrapingConfigObject = { [key: string]: string };
-
-function processScrapingParameters(parameters: string): ScrapingConfigObject {
-    parameters = parameters.trim();
-    parameters = parameters.endsWith(",") ? parameters.slice(-1) : parameters;
-
-    return parameters.split(",").reduce((obj, param) => {
-        const [rawKey, rawValue] = param.split(":").map((s) => s.trim());
-
-        const key = rawKey.startsWith('"') && rawKey.endsWith('"') ? rawKey.slice(1, -1) : rawKey;
-        const value = rawValue.startsWith('"') && rawValue.endsWith('"') ? rawValue.slice(1, -1) : rawValue;
-
-        if (!key || !value) {
-            throw new Error("Invalid parameter format");
-        }
-
-        obj[key] = value;
-        return obj;
-    }, {} as ScrapingConfigObject);
-}
-
 export const createScrapingConfig = async (req: Request, res: Response) => {
     try {
         const { userId } = req.session;
@@ -35,7 +14,7 @@ export const createScrapingConfig = async (req: Request, res: Response) => {
         const config = new ScrapeConfig({
             userId,
             url,
-            scrapeParameters: processScrapingParameters(scrapeParameters),
+            scrapeParameters,
             scrapeIntervalMinute,
             timeToScrape: new Date(),
         });
--- backend/src/util/checkAndExecuteScrape.ts
+++ backend/src/util/checkAndExecuteScrape.ts
@@ -4,43 +4,60 @@ import NoteModel from "../models/obj";
 
 let scrapeTimeout: NodeJS.Timeout;
 
+type ScrapingConfigObject = { [key: string]: string };
+
+function processScrapingParameters(parameters: any[]): ScrapingConfigObject {
+
+    return parameters.reduce((obj, param) => {
+        const key = param.name;
+        const value = param.tag;
+
+        if (!key || !value) {
+            throw new Error("Invalid parameter format");
+        }
+
+        obj[key] = value;
+        return obj;
+    }, {} as ScrapingConfigObject);
+}
+
 const checkAndExecuteScrape = async () => {
-    // try {
-    //     const [currentScrape] = await scrapeConfig.find({}).sort({ timeToScrape: 1 }).limit(1).exec();
+    try {
+        const [currentScrape] = await scrapeConfig.find({}).sort({ timeToScrape: 1 }).limit(1).exec();
 
-    //     if (!currentScrape) {
-    //         console.log("No scheduled scrapes. Checking again in 10 second.");
-    //         setNextScrapeTimeout(10 * 1000);
-    //     } else if (currentScrape.timeToScrape.getTime() <= Date.now()) {
-    //         console.log("Scraping for:", currentScrape.url, "at time:", new Date().toLocaleString("en-US", { timeZone: "America/Chicago" }));
+        if (!currentScrape) {
+            console.log("No scheduled scrapes. Checking again in 10 second.");
+            setNextScrapeTimeout(10 * 1000);
+        } else if (currentScrape.timeToScrape.getTime() <= Date.now()) {
+            console.log("Scraping for:", currentScrape.url, "at time:", new Date().toLocaleString("en-US", { timeZone: "America/Chicago" }));
 
-    //         const scrapedData = await scrapeWebsite(currentScrape.url, currentScrape.scrapeParameters);
-    //         if (scrapedData === undefined) {
-    //             console.log("Scrape failed.");
-    //         } else {
-    //             await NoteModel.findOneAndUpdate({ configId: currentScrape._id }, { $push: { scrapedData } });
-    //             console.log("Scrape successful.");
-    //         }
+            const scrapedData = await scrapeWebsite(currentScrape.url, processScrapingParameters(currentScrape.scrapeParameters));
+            if (scrapedData === undefined) {
+                console.log("Scrape failed.");
+            } else {
+                await NoteModel.findOneAndUpdate({ configId: currentScrape._id }, { $push: { scrapedData } });
+                console.log("Scrape successful.");
+            }
 
-    //         currentScrape.timeToScrape = new Date(Date.now() + currentScrape.scrapeIntervalMinute * 60000);
-    //         await currentScrape.save();
+            currentScrape.timeToScrape = new Date(Date.now() + currentScrape.scrapeIntervalMinute * 60000);
+            await currentScrape.save();
 
-    //         const [nextScrape] = await scrapeConfig.find({}).sort({ timeToScrape: 1 }).limit(1).exec();
-    //         setNextScrapeTimeout(nextScrape.scrapeIntervalMinute * 60000);
-    //     } else {
-    //         setNextScrapeTimeout(currentScrape.timeToScrape.getTime() - Date.now());
-    //     }
-    // } catch (error) {
-    //     console.error("Error in checkAndExecuteScrape:", error);
-    //     setNextScrapeTimeout(1 * 1000);
-    // }
+            const [nextScrape] = await scrapeConfig.find({}).sort({ timeToScrape: 1 }).limit(1).exec();
+            setNextScrapeTimeout(nextScrape.scrapeIntervalMinute * 60000);
+        } else {
+            setNextScrapeTimeout(currentScrape.timeToScrape.getTime() - Date.now());
+        }
+    } catch (error) {
+        console.error("Error in checkAndExecuteScrape:", error);
+        setNextScrapeTimeout(1 * 1000);
+    }
 };
 
 export const setNextScrapeTimeout = (interval: number) => {
-    // try {
-    //     clearTimeout(scrapeTimeout);
-    //     scrapeTimeout = setTimeout(checkAndExecuteScrape, interval);
-    // } catch {
-    //     setNextScrapeTimeout(1 * 1000);
-    // }
+    try {
+        clearTimeout(scrapeTimeout);
+        scrapeTimeout = setTimeout(checkAndExecuteScrape, interval);
+    } catch {
+        setNextScrapeTimeout(1 * 1000);
+    }
 };
