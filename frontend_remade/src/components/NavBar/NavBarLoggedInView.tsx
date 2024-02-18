6288e86b675aa632edf14c8330c89f04f045e974
Sofia-Ortega <s.ortega@tamu.edu>
2024-02-17 18:09:57 -0600

    Updated save button in addEditScrapeConfigDialog

--- frontend_remade/src/components/NavBar/NavBarLoggedInView.tsx
+++ frontend_remade/src/components/NavBar/NavBarLoggedInView.tsx
@@ -1,24 +1,29 @@
-import { Button } from "react-bootstrap";
 import * as ObjsApi from "../../network/objs_api";
 import "../../styles/Navbar.css";
 import MyButton from "../ui/MyButton";
 
 interface NavBarLoggedInViewProps {
-    onLogoutSuccessful: () => void;
+  onLogoutSuccessful: () => void;
 }
 
-const NavBarLoggedInView = ({ onLogoutSuccessful }: NavBarLoggedInViewProps) => {
-    async function logout() {
-        try {
-            await ObjsApi.logout();
-            onLogoutSuccessful();
-        } catch (error) {
-            console.error(error);
-            alert(error);
-        }
+const NavBarLoggedInView = ({
+  onLogoutSuccessful,
+}: NavBarLoggedInViewProps) => {
+  async function logout() {
+    try {
+      await ObjsApi.logout();
+      onLogoutSuccessful();
+    } catch (error) {
+      console.error(error);
+      alert(error);
     }
+  }
 
-    return <MyButton onClick={logout} variant="secondary">Log Out</MyButton>;
+  return (
+    <MyButton onClick={logout} variant="secondary">
+      Log Out
+    </MyButton>
+  );
 };
 
 export default NavBarLoggedInView;
