c9c31d781b2bb32576e77cdbaf9f4ad882d8db92
Sofia-Ortega <s.ortega@tamu.edu>
2024-02-17 18:02:26 -0600

    updated log in and sign up buttons in logged out view

--- frontend_remade/src/components/ui/MyButton.tsx
+++ frontend_remade/src/components/ui/MyButton.tsx
@@ -1,54 +1,62 @@
 import "../../styles/global.css";
+import "../../styles/utils.module.css";
 
 import { ReactNode } from "react";
 
 interface MyButtonProps {
-    variant?: "primary" | "secondary";
-    className?: string;
-    children: ReactNode;
-    href?: string;
-    onClick?: () => void;
-    disabled?: boolean;
+  variant?: "primary" | "secondary";
+  className?: string;
+  children: ReactNode;
+  href?: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  style?: {
+    [key: string]: any;
+  };
 }
 
 const PRIMARY_BTN_CLASS = ""; // default
 const SECONDARY_BTN_CLASS = "my-btn-secondary";
 
 const MyButton = ({
-    variant,
-    children,
-    href,
-    className,
-    onClick,
-    disabled,
+  variant,
+  children,
+  href,
+  className,
+  onClick,
+  disabled,
+  style,
 }: MyButtonProps) => {
-    let myClass: string = PRIMARY_BTN_CLASS;
+  let myClass: string = PRIMARY_BTN_CLASS;
 
-    if (variant == "primary") {
-        myClass = PRIMARY_BTN_CLASS;
-    } else if (variant == "secondary") {
-        myClass = SECONDARY_BTN_CLASS;
+  if (variant == "primary") {
+    myClass = PRIMARY_BTN_CLASS;
+  } else if (variant == "secondary") {
+    myClass = SECONDARY_BTN_CLASS;
+  }
+
+  const handleClick = () => {
+    if (href) {
+      window.location.href = href;
     }
 
-    const handleClick = () => {
-        if (href) {
-            window.location.href = href;
-        }
+    if (onClick) {
+      onClick();
+    }
+  };
 
-        if (onClick) {
-            onClick();
-        }
-    };
-
-    return (
-        <button
-            className={`${myClass} ${className}`}
-            onClick={handleClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <div>
+      <button
+        className={`${myClass} ${className}`}
+        onClick={onClick}
+        disabled={disabled}
+        style={style}
+      >
+        {children}
+      </button>
+    </div>
+  );
 };
 
 export default MyButton;
