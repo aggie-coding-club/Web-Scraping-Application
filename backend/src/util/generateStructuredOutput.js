09823753b59823e6bca47f91d5a014ad449d0945
cats256 <59489624+cats256@users.noreply.github.com>
2024-03-26 01:28:44 -0500

    Minor changes

--- backend/src/util/generateStructuredOutput.js
+++ backend/src/util/generateStructuredOutput.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const OpenAI = require("openai");
 const tiktoken = require("tiktoken");
+// const { convert } = require('html-to-text');
+// const axios = require('axios');
+
+const puppeteer = require('puppeteer');
 
 // TODO: Fix the import and export
 // TODO: Allows for categories that use more than one token
@@ -10,60 +14,143 @@ const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const enc = tiktoken.encoding_for_model("gpt-3.5-turbo-0125");
 
 const transcribed_image = `
-Exil - Hiboky
+Skip navigation
+Search
 
-Romain Spiteri
+
+
+Sign in
+
+
+0:02 / 1:00:09
+
+•
+My Soul
+
+The Best Of July (The Best Collection)
+
+July Official
+62K subscribers
+
 Subscribe
-31K subscribers
 
-10M views 5 years ago
-It's my first song in this channel. I hope you will love it
-You can stream this song on : ...more
+14K
 
-351K
 
-Share Download ...
+Share
 
-Next: 2 Hour Beautiful Piano Music for Studying an...
-Instrumental Music Full Final List - 3 / 316
+Save
 
-All Melodies Soundtracks Beats Ani
+1.3M views  6 years ago
+- Track List  -
 
-Discover how an education at
-Texas A&M RELLIS can help y...
-Texas A&M-RELLIS
-54K views
-Sponsored
+00:00 ~ 03:51 - My Soul …
 
-[slowed+reverb] Exil - Hiboky (1
-hour)
-aesthetic nightcore
-45K views 3 years ago
+44:08
+NOW PLAYING
+The Best Of July♪
+Moody
+38K views 4 years ago
 
-Shorts remixing this video
 
-8,591 Comments = Sort by
+46:41
+NOW PLAYING
+Best of Danbi - Piano Greatest Hits
+Nguyễn Việt Hoàng
+61K views 7 years ago
 
-Add a comment...
 
-@hornyhippo 4 years ago
-1:07 is probably what ur looking for
-10K
-Reply
-     161 replies
+Mix
+NOW PLAYING
+Mix - July Official
+More from this channel for you
 
-@vantablack8258 3 years ago (edited)
-"This is the story you started, Eren."
+1:00:46
+NOW PLAYING
+Beautiful Piano Music & Best Sad Music & Sleep Music
+Tido Kang
+6.1M views 5 years ago
 
--Armin
-4K
-Reply
-     40 replies
 
-@naija9031 1 year ago (edited)
-"People's lives don't end when they die, it ends when they lose faith."
+3:18:14
+NOW PLAYING
+Cozy Spring Terrace With Gentle Sunlight In The Morning Focus Work | Smooth Piano Jazz Music
+Mellow Coffee
+328 views 1 day ago
+New
 
-Type here to search
+
+1:12:41
+NOW PLAYING
+Music that makes you feel motivated and ready for your day🌱 | Work with me ☀️
+Kath Chill
+1K views 13 days ago
+
+
+3:48:59
+NOW PLAYING
+Beautiful Relaxing Music - Stop Overthinking, Stress Relief Music, Sleep Music, Calming Music #29
+Healing Soul
+431K views Streamed 4 months ago
+
+
+44:27
+NOW PLAYING
+DJ Okawari 피아노곡 모음(a collection of personal favorite DJ Okawari piano pieces | 好きなDJ Okawariピアノ曲)
+Eden Aria & Music
+789K views 3 years ago
+
+My Soul - July (줄라이) | 1시간 피아노 반복 | 공부, 작업, 수면, 휴식
+Salt Piano
+23K views 2 years ago
+
+[Playlist] 듣기만해도 심장이 콩닥콩닥 설레는 노래 모음 | 귓구녕이감동
+귓구녕이감동
+588K views 1 year ago
+
+Music heals the heart and blood vessels, Calming music restores the nervous system, relaxing #2
+Enjoy Life
+1.6M views Streamed 5 months ago
+
+3 Hour Long | Piano Music & Rain Sounds | Audiovisual ASMR |
+소디의 음악공방 SodyMusic
+21M views 4 years ago
+
+𝘖𝘊𝘛𝘖𝘉𝘌𝘙
+양파
+14K views 2 years ago
+
+Lofi With My Cat || Tropical Room & Cat 🪴🌱🐱 Chill/Sleep/Healing [ Lofi Mix - Lofi Songs ]
+Lofi With My Cat
+476K views Streamed 5 months ago
+
+𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭 "이 노래 뭐야" 첫 곡 멜로디 시작이 너무 맘에 드네
+기분 재생목록
+5.7K views 2 days ago
+New
+
+Beautiful Relaxing Music - Stop Overthinking, Stress Relief Music, Sleep Music, Calming Music #51
+Tranquil Paradise
+209K views Streamed 5 months ago
+
+🌼뉴욕의 꽃집에서 들려오는 산뜻한 재즈플레이리스트 l 𝐟𝐥𝐨𝐰𝐞𝐫 𝐒𝐡𝐨𝐩 𝐉𝐚𝐳𝐳 l𝙍𝙚𝙡𝙖𝙭𝙞𝙣𝙜 𝙅𝙖𝙯𝙯 𝙋𝙞𝙖𝙣𝙤 𝙈𝙪𝙨𝙞𝙘 𝙛𝙤𝙧 𝙎𝙩𝙤𝙧𝙚💕
+MONKEY BGM
+980 watching
+LIVE
+
+
+(no midroll) Kamado Tanjirou no Uta (piano 1hour 30minutes) - Demon Slayer ED : relaxing dreaming
+Emotional_Shine
+1.1M views 3 years ago
+
+우린 어둠이었으나 | 기도 | 묵상 | 큐티 | 휴식 | piano worship | ccm bgm
+보라수피아 borasupia
+166 views 1 day ago
+New
+
+[Playlist] 첫소절 듣고 극락 갔다 온 썰 푼다 | 국내 알앤비 노래모음 플레이리스트
+핑프
+201K views 11 months ago
 `;
 
 const yt_data = {
@@ -85,35 +172,27 @@ const yt_data = {
       type: "string",
       description: "The date the video was posted",
     },
-    comments: {
-      type: "array",
-      items: {
-        type: "object",
-        properties: {
-          author: {
-            type: "string",
-            description: "The author of the comment",
-          },
-          text: {
-            type: "string",
-            description: "The text of the comment",
-          },
-          date: {
-            type: "string",
-            description: "The date the comment was posted",
-          },
-        },
-        required: ["author", "text", "date"],
-      },
-    },
+    // comments: {
+    //   type: "array",
+    //   items: {
+    //     type: "object",
+    //     properties: {
+    //       author: {
+    //         type: "string",
+    //         description: "The author of the comment",
+    //       },
+    //       text: {
+    //         type: "string",
+    //         description: "The text of the comment",
+    //       },
+    //       date: {
+    //         type: "string",
+    //         description: "The date the comment was posted",
+    //       },
+    //     },
+    //   },
+    // },
   },
-  required: [
-    "channel_name",
-    "view_count",
-    "subscribers",
-    "comments",
-    "posted_date",
-  ],
 }
 
 async function generate_structured_data(data_schema, input) {
@@ -121,8 +200,8 @@ async function generate_structured_data(data_schema, input) {
     {
       type: "function",
       function: {
-        name: "generate_structured_ouput",
-        description: "Generate structured output from unstructured data",
+        name: "hallucination_is_bad",
+        description: "Do Not Hallucinate",
         parameters: data_schema,
       },
     },
@@ -222,13 +301,51 @@ async function LLM_classification(categories, input) {
   return classProbs;
 }
 
-generate_structured_data(yt_data, transcribed_image).then((res) => {
-  console.log(res);
-});
+(async () => {
+  const browser = await puppeteer.launch({
+      headless: "new"
+  });
+  const page = (await browser.pages())[0];
+  await page.goto('https://www.youtube.com/watch?v=Hm587hwUQmw&list=PLCkM3epMglyknX6N-tJ4vi-kML_c-cVy2', {
+    waitUntil: 'networkidle0'
+  });
+  const extractedText = await page.$eval('*', (el) => el.innerText);
 
-LLM_classification(["Yes", "No"], "Is this sarcasm? 'Is it time for your medication or mine?'").then((res) => {
-  console.log(res);
-});
+  // const extractedText = await page.$eval('*', (el) => {
+  //     const selection = window.getSelection();
+  //     const range = document.createRange();
+  //     range.selectNode(el);
+  //     selection.removeAllRanges();
+  //     selection.addRange(range);
+  //     return window.getSelection().toString();
+  // });
+
+  await browser.close();
+  console.log(extractedText);
+  console.log(await (generate_structured_data(yt_data, extractedText)));
+})();
+
+// axios.get('http://example.com').then((response) => {
+//   const text = convert(response.data);
+//   console.log(text);
+// });
+
+// axios.get('https://www.youtube.com/watch?v=Hm587hwUQmw&list=PLCkM3epMglyknX6N-tJ4vi-kML_c-cVy2').then((response) => {
+//   const text = convert(response.data);
+//   console.log(text);
+//   generate_structured_data(yt_data, text).then((res) => {
+//     console.log(res);
+//   });  
+// });
+
+
+// generate_structured_data(yt_data, transcribed_image).then((res) => {
+//   console.log(res);
+// });
+
+// LLM_classification(["Yes", "No"], "Is this sarcasm? 'Is it time for your medication or mine?'").then((res) => {
+//   console.log(res);
+// });
 
 // LLM_classification(["Yes", "No"], `Is this sarcasm? 'He's so smart his IQ is the same as a gorrila'`).then((res) => {
 //   console.log(res);
@@ -349,3 +466,4 @@ LLM_classification(["Yes", "No"], "Is this sarcasm? 'Is it time for your medicat
 // LLM_classification(["Better", "Worse"], "Naruto compared to Dragon Balls").then((res) => {
 //   console.log(res);
 // });
+
